52b5177d83c339a4ced4ba244979fad84a9e4835
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2025-08-02 14:13:00 -0300

    modulo1

---
+++ cursos/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Base(models.Model):
+  criacao = models.DateTimeField(auto_now_add=True)
+  atualizacao = models.DateTimeField(auto_now=True)
+  ativo = models.BooleanField(default=True)
+
+  class Meta:
+    abstract = True
+
+class Curso(Base):
+  titulo = models.CharField(max_length=255)
+  url = models.URLField(unique=True)
+
+  class Meta:
+    verbose_name = 'Curso'
+    verbose_name_plural = 'Cursos'
+
+  def __str__(self):
+    return self.titulo
+
+class Avaliacao(Base):
+  curso = models.ForeignKey(Curso, on_delete=models.CASCADE, related_name='avaliacoes')
+  nome = models.CharField(max_length=255)
+  email = models.EmailField()
+  comentario = models.TextField(blank=True, null=True, default='')
+  avalicao = models.DecimalField(max_digits=2, decimal_places=1)
+
+  class Meta:
+    verbose_name = 'Avaliação'
+    verbose_name_plural = 'Avaliações'
+    unique_together = ('email', 'curso')
+
+  def __str__(self):
+    return f'{self.nome} avaliou o curso {self.curso.titulo} com nota {self.avalicao}'
