471b64a6a014015f4b3e6d18afde368115fbe536
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2025-08-03 02:16:19 -0300

    validation

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
+from django.db.models import Avg
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
   class Meta:
@@ -8,6 +9,11 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
     }
     model = Avaliacao
     fields = '__all__'
+
+  def validate_avalicao(self, valor):
+    if valor in range(1,6):
+      return valor
+    raise serializers.ValidationError('A avaliação deve ser entre 1 e 5.')
 
 class CursoSerializer(serializers.ModelSerializer):
 
@@ -20,8 +26,15 @@ class CursoSerializer(serializers.ModelSerializer):
   #3. Primary Key Relationship (mais performático)
   avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
-
+  media_avalicoes = serializers.SerializerMethodField()
 
   class Meta:
     model = Curso
-    fields = ('id','titulo', 'url', 'criacao', 'ativo' ,'avaliacoes')
+    fields = ('id','titulo', 'url', 'criacao', 'ativo' ,'avaliacoes', 'media_avalicoes')
+
+  def get_media_avalicoes(self, obj):
+    media = obj.avaliacoes.aggregate(Avg('avalicao')).get('avalicao__avg')
+    if media is None:
+      return 0
+    return round(media*2)/2
+  
--- escola/settings.py
+++ escola/settings.py
@@ -137,10 +137,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 2,
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle',
+        #'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '2/minute', #second, day, montyh, year
-        'user': '3/minute'
+        #'user': '3/minute'
     }
 }
