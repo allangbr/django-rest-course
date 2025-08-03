2a9afb5cb7d156647aa686b8bf31ef37d6b30824
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2025-08-03 00:55:21 -0300

    class6 - pagination

--- cursos/views.py
+++ cursos/views.py
@@ -44,18 +44,30 @@ class CursoViewSet(viewsets.ModelViewSet):
 
   @action(detail=True, methods=['get'])
   def avaliacoes(self, request, pk=None):
-    curso = self.get_object()
-    serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+    #curso = self.get_object()
+
+    #Pagination
+    self.pagination_class.page_size = 1
+    avaliacoes = Avaliacao.objects.filter(curso_id=pk)
+    page = self.paginate_queryset(avaliacoes)
+
+    if page is not None:
+      serializer = AvaliacaoSerializer(page, many=True)
+      return self.get_paginated_response(serializer.data)
+
+    serializer = AvaliacaoSerializer(avaliacoes.all(), many=True)
     return Response(serializer.data)
 
 #VIEWSET PADRAO
-'''
+
 class AvaliacaoViewSet(viewsets.ModelViewSet):
   queryset = Avaliacao.objects.all()
   serializer_class = AvaliacaoSerializer
-'''
+
 
 # VIEWSET PERSONALIZADO
+'''
 class AvaliacaoViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
   queryset = Avaliacao.objects.all()
   serializer_class = AvaliacaoSerializer
+'''
--- escola/settings.py
+++ escola/settings.py
@@ -122,3 +122,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
