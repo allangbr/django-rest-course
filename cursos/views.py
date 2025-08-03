bc347990c14d40a16ac649369bcfd53f4f8aedcc
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2025-08-02 23:41:13 -0300

    class4

--- cursos/views.py
+++ cursos/views.py
@@ -1,33 +1,30 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 # Create your views here.
-class CursoAPIView(APIView):
-  def get(self, request):
-    cursos = Curso.objects.all()
-    serializer = CursoSerializer(cursos, many=True)
-    return Response(serializer.data)
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+  #lookup_field = 'id'
 
-  def post(self, request):
-    serializer = CursoSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CursosAPIView(generics.ListCreateAPIView):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
 
-class AvaliacaoAPIView(APIView):
-  def get(self, request):
-    avaliacoes = Avaliacao.objects.all()
-    serializer = AvaliacaoSerializer(avaliacoes, many=True)
-    return Response(serializer.data)
 
-  def post(self, request):
-    serializer = AvaliacaoSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
+  lookup_field = 'id'
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
+
+  # def get_queryset(self):
+  #   curso_id = self.kwargs.get('curso_id')
+  #   if curso_id:
+  #     return self.queryset.filter(curso_id=curso_id)
+  #   return self.queryset
